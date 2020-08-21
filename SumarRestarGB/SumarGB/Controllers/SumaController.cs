d147483fbfcc63c7caa3af84159157760b73d382
GEAN MARCO VALVERDE ARANA <a2019114321@estudiantes.upsa.edu.bo>
2020-08-21 19:02:34 -0400

    Primer Modelo

--- SumarRestarGB/SumarGB/Controllers/SumaController.cs
+++ SumarRestarGB/SumarGB/Controllers/SumaController.cs
@@ -7,19 +7,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SumarGB.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     [Route("api/[controller]")]
     [ApiController]
     public class SumaController : ControllerBase
     {
         [HttpGet]
+        [Route("Add")]
         public int Add(int a, int b)
         {
             return a + b;
         }
         [HttpPost]
+        [Route("Multiply")]
+        public int Mul( int a, int b)
+        {
+            return a * b;
+        }
+        [HttpGet]
+        [Route("Add")]
         public int Add2([FromHeader] int a, [FromHeader] int b)
         {
             return a + b;
+        }
+        [HttpGet]
+        [Route("Multiply")]
+        public int Mul2([FromHeader] int a, [FromHeader] int b)
+        {
+            return a * b;
         }
     }
 }
